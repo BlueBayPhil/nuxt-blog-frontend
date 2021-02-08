bda8c309f9d314f5b65bced6c812fa32c67e9cb0
Phil Davies <phil.davies@bluebaytravel.co.uk>
2021-02-08 10:09:02 +0000

    Extracted editor into it's own component from create page.

--- pages/create.vue
+++ pages/create.vue
@@ -1,16 +1,6 @@
 <template>
   <div>
-    <form method="post" @submit.prevent="savePost()">
-      <div class="form-group">
-        <label for="titleField">Title</label>
-        <input type="text" v-model="postTitle" id="titleField" maxlength="100" placeholder="My awesome title...">
-      </div>
-      <div class="form-group">
-        <label for="contentField">Content</label>
-        <textarea id="contentField" class="form-control" v-model="postContent" style="height: 65vh"
-                  placeholder="Free your mind..."></textarea>
-      </div>
-    </form>
+    <Editor />
   </div>
 </template>
 
@@ -18,22 +8,6 @@
 export default {
   middleware: 'auth',
   computed: {
-    postTitle: {
-      get() {
-        return this.$store.state.post.title;
-      },
-      set(value) {
-        this.$store.commit('post/title', value);
-      }
-    },
-    postContent: {
-      get() {
-        return this.$store.state.post.content;
-      },
-      set(value) {
-        this.$store.commit('post/content', value);
-      }
-    },
     posting() {
       return this.$store.state.post.posting;
     }
